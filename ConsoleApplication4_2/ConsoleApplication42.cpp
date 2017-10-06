93daccac8234113c5b4617760c85d8013531618e
ponychan <rzsckbmx@163.com>
2017-10-06 14:01:00 +0800

    4.2.cpp
    point类的完整程序

--- ConsoleApplication4_2/ConsoleApplication42.cpp
+++ ConsoleApplication4_2/ConsoleApplication42.cpp
@@ -1,11 +1,51 @@
 // ConsoleApplication42.cpp: 定义控制台应用程序的入口点。
-//
+//4.2.cpp
 
 #include "stdafx.h"
+#include<iostream>
+using namespace std;
 
+class point {
+public:
+	point(int xx = 0, int yy = 0) {
+		x = xx;
+		y = yy;
+	}
+	point(point &p);
+	int getx() {
+		return x;
+	}
+	int gety() {
+		return y;
+	}
+private:
+	int x, y;
+};
+
+//成员函数的实现
+point::point(point &p) {
+	x = p.x;
+	y = p.y;
+	cout << "calling the copy constructor" << endl;
+}
+
+void fun1(point p) {
+	cout << p.getx() << endl;
+}
+
+point fun2() {
+	point a(1, 2);
+	return a;
+}
 
 int main()
 {
-    return 0;
+	point a(4, 5);
+	point b = a;
+	cout << b.getx() << endl;
+	fun1(b);
+	b = fun2();
+	cout << b.getx() << endl;
+	return 0;
 }
 
